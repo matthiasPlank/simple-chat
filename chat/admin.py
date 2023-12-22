4bc04d09fd799e4de4a14328174479e294f36510
Matthias Plank <matthias.plank@gmx.at>
2023-12-22 21:42:17 +0100

    add messages and chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from chat.models import Message, Chat
+
+class MessageAdmin(admin.ModelAdmin):    
+    fields = ('text','created_at', 'author', 'receiver', 'chat')    
+    list_display = ('created_at', 'author', 'text', 'receiver',  'chat')    
+    search_fields = ('text',)
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from chat.models import Message, Chat
+
 # Create your views here.
 def index(request): 
-    return render(request, 'chat/index.html')
+    print(request.method)
+    if request.method == 'POST':
+        print("Received Data:" + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'] , chat=myChat , author=request.user , receiver=request.user); 
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html' , {'messages':chatMessages})
