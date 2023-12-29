e904fec2413d953b9a4f1e8bfb8c8b6ab66b6bc8
Matthias Plank <matthias.plank@gmx.at>
2023-12-29 12:37:10 +0100

    update Timezone

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from datetime import date , datetime
 from django.conf import settings
-from django.utils.timezone import now
+from django.utils import timezone
+
 
 
 # Create your models here.
@@ -12,7 +13,7 @@ class Chat(models.Model):
     
 class Message(models.Model):
     text = models.CharField(max_length=500) 
-    created_at = models.DateTimeField(default=now , blank=True)
+    created_at = models.DateTimeField(default=timezone.now , blank=True)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE , related_name='chat_message_set', default=None, blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE , related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE , related_name='receiver_message_set')
