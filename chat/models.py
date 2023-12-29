ffabf2e6d161e39225a149cd7a96cbdf14d6f7cd
Matthias Plank <matthias.plank@gmx.at>
2023-12-29 12:24:34 +0100

    add Time to Message

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from datetime import date
+from datetime import date , datetime
 from django.conf import settings
+from django.utils.timezone import now
+
 
 # Create your models here.
 
@@ -10,7 +12,7 @@ class Chat(models.Model):
     
 class Message(models.Model):
     text = models.CharField(max_length=500) 
-    created_at = models.DateField(default=date.today)
+    created_at = models.DateTimeField(default=now , blank=True)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE , related_name='chat_message_set', default=None, blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE , related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE , related_name='receiver_message_set')
